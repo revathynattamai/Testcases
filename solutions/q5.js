8c18adcac7558dcf31ee1108b994b0858a591f13
revathynattamai <revathy.ng@gmail.com>
2017-05-21 10:51:41 +0530

    q5 passed

--- solutions/q5.js
+++ solutions/q5.js
@@ -1,8 +1,16 @@
 //Q5. Write a function which takes array as argument and remove duplicates elements and returns a new array.
 // INPUT
-var arr=[1,1,2,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+var arr = [1, 1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
 
-function removeDuplicates(arr){
+function removeDuplicates(arr) {
   //Write your code here
+  var res = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (res.indexOf(arr[i]) == -1) {
+      res.push(arr[i]);
+    }
+  }
+  return (res);
 }
+
 module.exports = removeDuplicates;
