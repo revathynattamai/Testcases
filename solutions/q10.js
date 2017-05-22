bade0ed56455e141695acf74185c9cb4df3cc43e
revathynattamai <revathy.ng@gmail.com>
2017-05-22 16:25:35 +0530

    q10 passed

--- solutions/q10.js
+++ solutions/q10.js
@@ -10,17 +10,43 @@ Range   	     Grade	Rating
 */
 
 const studentDetails = [
-  {name:'David',marks:80},
-  {name:'Vinoth',marks:77},
-  {name:'Divya',marks:88},
-  {name:'Ishitha',marks:95},
-  {name:'Thomas',marks:68}
+  { name: 'David', marks: 80 },
+  { name: 'Vinoth', marks: 77 },
+  { name: 'Divya', marks: 88 },
+  { name: 'Ishitha', marks: 95 },
+  { name: 'Thomas', marks: 68 }
 ];
 
-let studentsGradeRatingArr = []
+let studentsGradeRatingArr = [];
 
 //Write your code here
 //
 
+
+  for (let i = 0; i < studentDetails.length; i++) {
+    let a = studentDetails[i].marks;
+    const newobj = {};
+    newobj.name = studentDetails[i].name;
+    if (a >= 61 && a <= 70) {
+      newobj.grade = "D";
+      newobj.rating = "Fair";
+    }
+
+    else if (a >= 71 && a <= 80) {
+      newobj.grade = "C";
+      newobj.rating = "Average";
+    }
+
+    else if (a >= 81 && a <= 90) {
+      newobj.grade = "B";
+      newobj.rating = "Good";
+    }
+
+    else {
+      newobj.grade = "A";
+      newobj.rating = "Excellent";
+    }
+    studentsGradeRatingArr.push(newobj);
+  }
 
 module.exports = studentsGradeRatingArr;
