282c4594b10457040cb63f4ee61d8cdcc720e3ae
revathynattamai <revathy.ng@gmail.com>
2017-05-22 12:49:07 +0530

    q13 passed

--- solutions/q13.js
+++ solutions/q13.js
@@ -1,6 +1,12 @@
 //Q13. Write a program to list (Fibonacci) series till a given number?
 
-fibonacci = function(numMax, done){
-//Write your code here
+fibonacci = function (numMax, done) {
+    //Write your code here
+    const res = [0, 1];
+    for (let i = 0; i < numMax; i++) {
+        var b = res[i] + res[i + 1];
+        res.push(b);
+    }
+    done(res);
 }
 module.exports = fibonacci;
